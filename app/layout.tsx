46df39e55eea196976aa768ff13ead36ec2c2586
socialclubfinder <420ganjacoin@gmail.com>
2025-03-29 20:36:36 +0100

    Fixed icon component types and updated company logos

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] });
 
