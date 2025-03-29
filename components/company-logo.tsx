aab58652c75c3a25d4e400ba3d44c3b4377e8edc
socialclubfinder <420ganjacoin@gmail.com>
2025-03-29 19:53:34 +0100

    Fixed icon component types and updated company logos

--- components/company-logo.tsx
+++ components/company-logo.tsx
@@ -1,6 +1,4 @@
 "use client"
-
-import { useState } from 'react';
 import { 
   Leaf, 
   Pill, 
@@ -20,23 +18,24 @@ interface CompanyLogoProps {
   name: string;
 }
 
-const companyIcons: Record<string, React.ComponentType> = {
-  'Bloomwell': Flower2,
-  'CannGo': Leaf,
-  'Quick-Green': TreePine,
-  'DoktorABC': Stethoscope,
-  'DrAnsay': Heart,
-  'Telecan': Activity,
-  'enmedify': Cross,
-  'nowomed': Pill,
-  'Kanna Medics': Star,
-  'CannCura': Sun,
-  'GreenMedical': Sparkles,
+// Typing each icon explicitly
+const companyIcons: Record<string, React.ComponentType<React.SVGProps<SVGSVGElement>>> = {
+  'Bloomwell': Flower2 as React.ComponentType<React.SVGProps<SVGSVGElement>>,
+  'CannGo': Leaf as React.ComponentType<React.SVGProps<SVGSVGElement>>,
+  'Quick-Green': TreePine as React.ComponentType<React.SVGProps<SVGSVGElement>>,
+  'DoktorABC': Stethoscope as React.ComponentType<React.SVGProps<SVGSVGElement>>,
+  'DrAnsay': Heart as React.ComponentType<React.SVGProps<SVGSVGElement>>,
+  'Telecan': Activity as React.ComponentType<React.SVGProps<SVGSVGElement>>,
+  'enmedify': Cross as React.ComponentType<React.SVGProps<SVGSVGElement>>,
+  'nowomed': Pill as React.ComponentType<React.SVGProps<SVGSVGElement>>,
+  'Kanna Medics': Star as React.ComponentType<React.SVGProps<SVGSVGElement>>,
+  'CannCura': Sun as React.ComponentType<React.SVGProps<SVGSVGElement>>,
+  'GreenMedical': Sparkles as React.ComponentType<React.SVGProps<SVGSVGElement>>,
 };
 
 export function CompanyLogo({ name }: CompanyLogoProps) {
   const IconComponent = companyIcons[name] || Leaf;
-  
+
   return (
     <div className="w-8 h-8 flex items-center justify-center bg-green-100 dark:bg-green-900 rounded-full">
       <IconComponent className="w-4 h-4 text-green-600 dark:text-green-400" />
