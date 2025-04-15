070ffa684ef9136e645cd5ca48e9457643ddd8db
socialclubfinder <420ganjacoin@gmail.com>
2025-04-15 22:26:41 +0200

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -7,43 +8,12 @@ const nextConfig = {
     unoptimized: true,
     domains: ['images.unsplash.com', 'favicon.ico']
   },
-  webpackBuildCache: false,
   compress: true,
   poweredByHeader: false,
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
-    optimizeCss: true,
-    legacyBrowsers: false,
-  },
-  headers: async () => {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-DNS-Prefetch-Control',
-            value: 'on'
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block'
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN'
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff'
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin'
-          }
-        ]
-      }
-    ];
+    optimizeCss: true
   }
 };
 
